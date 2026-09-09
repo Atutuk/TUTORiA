a5920f990775bb3305468f79503695ace0d4f062
Atutuk <mouravidalarthur@gmail.com>
2026-09-08 21:07:12 -0300

    Primeiro commit do TutorIA

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+const SUPABASE_URL = "https://zmrznwsniedtqifpasyw.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_fm1xgWm47mY52My9LkXWpQ_g1x1kwIz";
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+    );
